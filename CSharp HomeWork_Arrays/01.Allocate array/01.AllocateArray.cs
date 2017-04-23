60749934abaa4412472a246a587a80e1eea860ea
RadoslavGL <radoslavlgeorgiev@gmail.com>
2017-04-24 00:01:47 +0300

    01 re-done

---
+++ CSharp HomeWork_Arrays/01.Allocate array/01.AllocateArray.cs
@@ -0,0 +1,25 @@
+﻿//Write a program that allocates array of N integers, initializes each element by its index multiplied by 5
+//and the prints the obtained array on the console.
+
+//Print the obtained array on the console. Each number should be on a new line.
+
+namespace _01.Allocate_array
+{
+    using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] numbers = new int[n];
+            int multiplier = 5;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = i * multiplier;
+            }
+
+            Console.WriteLine(string.Join("\n", numbers));
+        }
+    }
+}
