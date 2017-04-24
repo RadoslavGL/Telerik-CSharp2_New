431efcdf8be2206ca96e7bf5fe20e76e7160fd54
RadoslavGL <radoslavlgeorgiev@gmail.com>
2017-04-24 12:43:00 +0300

    hope it works X2

---
+++ CSharp HomeWork_Arrays/03.Compare char arrays/03.CompareCharArrays.cs
@@ -0,0 +1,58 @@
+﻿//Write a program that compares two char arrays lexicographically (letter by letter).
+//On the first line you will receive the first char array as a string
+//On the second line you will receive the second char array as a string
+
+//Print < if the first array is lexicographically smaller
+//Print > if the second array is lexicographically smaller
+//Print = if the arrays are equal
+
+namespace _03.Compare_char_arrays
+{
+    using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] charArraysOne = Console.ReadLine().ToCharArray();
+            char[] charArrayTwo = Console.ReadLine().ToCharArray();
+
+            bool isEqual = true;
+
+            for (int i = 0; i < Math.Min(charArraysOne.Length, charArrayTwo.Length); i++)
+            {
+                if (charArraysOne[i] > charArrayTwo[i])
+                {
+                    Console.WriteLine(">");
+                    isEqual = false;
+                    break;
+                }
+
+                else if (charArraysOne[i] < charArrayTwo[i])
+                {
+                    Console.WriteLine("<");
+                    isEqual = false;
+                    break;
+                }
+                
+            }
+
+            if (isEqual)
+            {
+                if (charArraysOne.Length > charArrayTwo.Length)
+                {
+                    Console.WriteLine(">");
+                }
+                else if (charArraysOne.Length < charArrayTwo.Length)
+                {
+                    Console.WriteLine("<");
+                }
+                else
+                {
+                    Console.WriteLine("=");
+                }
+            }
+           
+
+        }
+    }
+}
---
+++ CSharp HomeWork_Arrays/04.Maximal sequence/04.MaximalSequence.cs
@@ -0,0 +1,47 @@
+﻿//Write a program that finds the length of the maximal sequence of EQUAL elements in an array of N integers.
+//On the first line you will receive the number N
+//On the next N lines the numbers of the array will be given
+
+//Print the length of the maximal sequence
+
+namespace _04_Maximal_sequence
+{
+    using System;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int[] numbers = new int[N];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            int count = 1;
+            int maxCount = 1;
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i-1] == numbers[i])
+                {
+                    count++;
+
+                    if (maxCount < count)
+                    {
+                        maxCount = count;
+                    }
+
+                }
+
+                else
+                {
+                    count = 1;
+                }
+            }
+
+            Console.WriteLine(maxCount);
+
+        }
+    }
+}
