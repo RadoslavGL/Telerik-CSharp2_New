0b6c08969833758b7af56d261081ebd5adb760b3
RadoslavGL <radoslavlgeorgiev@gmail.com>
2017-05-01 10:51:09 +0300

    task 18 added

--- CSharp HomeWork_Arrays/18.Remove Elements From Array/18.RemoveElementsFromArray.cs
+++ CSharp HomeWork_Arrays/18.Remove Elements From Array/18.RemoveElementsFromArray.cs
@@ -20,8 +20,38 @@ namespace _18.Remove_Elements_From_Array
     {
         static void Main(string[] args)
         {
+            int N = Convert.ToInt32(Console.ReadLine());
+            int[] numbers = new int[N];
+            InputValuesToArray(numbers);
 
+            int[] maxSub = new int[numbers.Length];
+            maxSub = Enumerable.Repeat(1, numbers.Length).ToArray();
+            FillMaxSub(numbers, maxSub);
 
+            Console.WriteLine(numbers.Length - maxSub.Max());
+
+        }
+
+        private static void InputValuesToArray(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        private static void FillMaxSub(int[] numbers, int[] maxSub)
+        {
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if ((numbers[i] >= numbers[j]) && (maxSub[i] < (maxSub[j] + 1)))
+                    {
+                        maxSub[i] = maxSub[j] + 1;
+                    }
+                }
+            }
         }
     }
 }
