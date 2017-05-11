451d6f7ca17fbce7d00aebf2708e28d6fa8779b8
RadoslavGL <radoslavlgeorgiev@gmail.com>
2017-05-11 14:54:34 +0300

    Fill the matrix up to matrix C

--- CSharp Homework_Multidimensional-Arrays/01.Fill the matrix/01.FillTheMatrix.cs
+++ CSharp Homework_Multidimensional-Arrays/01.Fill the matrix/01.FillTheMatrix.cs
@@ -17,24 +17,121 @@ namespace _01.Fill_the_matrix
     {
         static void Main(string[] args)
         {
-            int N = int.Parse(Console.ReadLine());
-            char letter = char.Parse(Console.ReadLine().ToLower());
+            //int N = int.Parse(Console.ReadLine());
+            //char letter = char.Parse(Console.ReadLine().ToLower());
+            int N = 4;
+            char letter = 'c';
 
             int[,] matrix = new int[N, N];
 
-            int startValue = 1;
-
+            //first case - increasing towards the bottom on each column
             if (letter == 'a')
             {
-                for (int row = 0; row < matrix.GetLength(0); row++)
+
+                int count = 0;
+
+                for (int row = 0; row < N; row++)
                 {
-                    for (int col = 0; col < matrix.GetLength(1); col++)
+                    count++;
+                    for (int col = 0; col < N; col++)
                     {
-                        matrix[row, col] = startValue + row + col + 3 * col;
+                        matrix[row, col] = count;
+                        count += N;
+                    }
+                    count -= N * N;
+                }
+
+
+            }
+            //second case - like a caterpillar
+            else if (letter == 'b')
+            {
+
+                int count = 1;
+                int currentCount = 1;
+
+                for (int col = 0; col < N; col++)
+                {
+                    //switches between accending and descending the same N numbers vertically;
+                    //if we change to matrix[col, row] we will get the same thing, but horizontally;
+                    for (int row = 0; row < N; row++)
+                    {
+                        if (col % 2 == 0)
+                        {
+                            matrix[row, col] = count;
+                            count++;
+                        }
+                        else
+                        {
+                            count--;
+                            matrix[row, col] = count;
+                            
+                        }
+
                     }
 
+                    //stores the value of the counter so that it's used for 
+
+                    if (col % 2 == 0)
+                    {
+                        currentCount = count;
+                        currentCount += N;
+                        count = currentCount;
+                    }
+                    else if (col % 2 != 0)
+                    {
+                        count = currentCount;
+
+                    }
                 }
             }
+
+            //7   11  14  16
+            //4   8   12  15
+            //2   5   9   13
+            //1   3   6   10
+
+            else if (letter == 'c')
+            {
+                int rows = 0;
+                int cols = 0;
+                int value = 1;
+
+                //populate values under the main diagonal
+
+                for (int i = N - 1; i >= 0; i--)
+                {
+                    rows = i;
+                    cols = 0;
+                    while (rows < N && cols < N)
+                    {
+                        matrix[rows, cols] = value;
+                        rows++;
+                        cols++;
+                        value++;
+                    }
+                }
+
+                //populate values over the main diagonal
+
+                for (int j = 1; j < N; j++)
+                {
+                    rows = j;
+                    cols = 0;
+                    while (rows < N && cols < N)
+                    {
+                        matrix[cols, rows] = value;
+                        cols++;
+                        rows++;
+                        value++;
+                    }
+                }
+
+
+
+            }
+           
+
 
             //print
             PrintMatrix(matrix);
